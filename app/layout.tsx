9dcef663e16fdd3e6bd9e7f12d31b135275addf1
Shifa Tazeen Shaikh <shifatazeenshaikh@Shifas-MacBook-Air.local>
2026-08-11 13:17:30 +0530

    google analytics setup code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import FloaterBars from "@/components/FloaterBars";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "KRM Healthcare | Complete Laboratory Solutions",
@@ -23,6 +24,20 @@ export default function RootLayout({
       <body>
         {children}
         <FloaterBars />
+         <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WNG5JH2B16"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-WNG5JH2B16');
+          `}
+        </Script>
       </body>
     </html>
   );
